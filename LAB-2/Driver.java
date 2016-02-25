c1b69a860964c5bd1555c9ff08bcd226ede46d06
bhargavimadhunala <bhargavi.madhunala1@marist.edu>
2016-02-24 22:03:06 -0500

    this is my LAB-2 Assignment
    This assignment is used to generate AES algorithm .

---
+++ LAB-2/Driver.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Driver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class Driver
+{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        String thekey=input.nextLine();
+        System.out.println("your key is:" +thekey);
+        AES aes;
+        aes=new AES();
+    }
+}
